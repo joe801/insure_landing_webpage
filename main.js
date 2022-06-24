388b56905b45808ce9cd6631c5f740cffc524144
joecrypt <oyakhilomejoseph12@gmail.com>
2022-06-24 15:06:18 +0100

    removed unused codes

--- main.js
+++ main.js
@@ -4,16 +4,7 @@ let active = document.getElementById("active");
 let bar = document.querySelector(".bar");
 let cancel = document.querySelector(".cancel")
 
-/*
-document.getElementById("nav-btn").onclick=function(){
-	main.style.display = "none";
-	footer.style.display = "none";
-	active.style.display = "flex";
-	bar.style.display = "none";
-	cancel.style.display = "block";
-} */
-
 $('#nav-btn').click(function(){
-		$('#navbar .title-tab').toggleClass("active");
-		$('#nav-btn i').toggleClass("active");
-	});
+	$('#navbar .title-tab').toggleClass("active");
+	$('#nav-btn i').toggleClass("active");
+});
